46b8d3961236fc9568a8e5ef80e221bb4b01f43b
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2025-02-02 20:21:01 +0700

    Implement protected routes for authenticated access and enhance Navbar authentication handling

--- src/learniumAI_frontend/src/App.tsx
+++ src/learniumAI_frontend/src/App.tsx
@@ -4,15 +4,23 @@ import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
 import { AuthProvider } from "./context/AuthContext";
 import SummaryQuiz from "./pages/SummaryQuiz";
+import ProtectedRoute from "./context/ProtectedRoute";
 
 function App() {
   return (
     <Router>
       <AuthProvider>
         <Routes>
+          {/* Halaman Home bisa diakses siapa saja */}
           <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/summary-quiz" element={<SummaryQuiz />} />
+
+          {/* Halaman yang memerlukan login */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/summary-quiz" element={<SummaryQuiz />} />
+          </Route>
+
+          {/* Halaman Not Found */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </AuthProvider>
