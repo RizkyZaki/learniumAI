8d58e8ee59d073d2ef2dd6800cd2cb554719f0db
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2025-02-02 20:12:42 +0700

    Implement logout confirmation dialog in Navbar and format Principal ID display in DashboardHeader

--- src/learniumAI_frontend/src/components/dashboard/DashboardHeader.tsx
+++ src/learniumAI_frontend/src/components/dashboard/DashboardHeader.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 
 interface DashboardHeaderProps {
-  principal: string | undefined; 
+  principal: string | undefined;
 }
 
 const DashboardHeader: React.FC<DashboardHeaderProps> = ({ principal }) => {
+  // Format Principal ID agar hanya menampilkan bagian awal dan akhir
+  const formatPrincipal = (id: string | undefined) => {
+    if (!id) return "";
+    const parts = id.split("-");
+    return `${parts[0]}...${parts[parts.length - 1]}`;
+  };
+
   return (
     <div className="mb-14 mt-32">
       {/* Header dengan gradasi warna */}
       <h1 className="text-4xl font-bold mb-2 bg-gradient-to-r from-[#FB928E] to-[#6F41FF] bg-clip-text text-transparent relative inline-block">
-        Welcome, Your Principal ID {principal}!{" "}
+        Welcome, Your Principal ID: {formatPrincipal(principal)}!{" "}
         <span className="text-[#FB928E]">👋</span>
         {/* Garis di bawah teks */}
         <img
